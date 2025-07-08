752a68ade8acf6af3e4acb06b1c2e034a2f199c7
Bharath <bharathdevang373@gmail.com>
2025-07-09 01:07:46 +0530

    Add: your commit message

---
+++ backend/index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+import authRoutes from "./routes/authRoutes.js";
+import budgetRoutes from "./routes/budgetRoutes.js";
+import transactionRoutes from "./routes/transactionRoutes.js";
+
+const app = express();
+const prisma = new PrismaClient();
+
+const allowedOrigins = [
+  "http://localhost:5173",
+];
+
+app.use(cors({ 
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true 
+}));
+
+dotenv.config();
+app.use(express.json());
+
+//routes
+app.use("/api/auth", authRoutes);
+app.use("/api/budget", budgetRoutes);
+app.use("/api/transaction", transactionRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+async function checkDatabaseConnection() {
+      try {
+            await prisma.$connect();
+            console.log("Successfully connected to the database!");
+      } catch (error) {
+            console.error("Error connecting to the database:", error.message);
+      } finally {
+            await prisma.$disconnect();
+      }
+}
+
+app.listen(PORT, () => {
+      checkDatabaseConnection();
+      console.log(`Server running on port ${PORT}`);
+});
---
+++ frontend/src/utils/config.js
@@ -0,0 +1,7 @@
+const API_BASE_URL = "https://localhost:5000/api";
+
+export const API_URLS = {
+  auth: `${API_BASE_URL}/auth`,
+  budget: `${API_BASE_URL}/budget`,
+  transaction: `${API_BASE_URL}/transaction`,
+}; 
